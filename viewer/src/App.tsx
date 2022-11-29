1504e34e62815298bba09e5b1c17dcf85d5a3a00
Matt Iannucci <matt.iannucci@rpsgroup.com>
2022-11-29 14:14:30 -0500

    Add map component

--- viewer/src/App.tsx
+++ viewer/src/App.tsx
@@ -1,9 +1,19 @@
+import { useRef } from "react";
+import Map from "./components/map";
 
 function App() {
+  const map = useRef<maplibregl.Map | null>(null);
+
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div className="h-screen w-screen">
+      <Map 
+        map={map} 
+        style='https://api.maptiler.com/maps/ocean/style.json?key=x5EfXrIDiRScOPCSUPJ6' 
+        viewport={{
+          center: [-71, 41],
+          zoom: 3,
+        }}/>
+    </div>
   )
 }
 
---
+++ viewer/src/components/map.tsx
@@ -0,0 +1,35 @@
+import maplibregl from "maplibre-gl";
+import { MutableRefObject, useEffect, useRef } from "react";
+import 'maplibre-gl/dist/maplibre-gl.css';
+
+interface MapProps {
+    map: MutableRefObject<maplibregl.Map | null>,
+    style: string,
+    viewport: {
+        center: [number, number], 
+        zoom: number,
+    }
+}
+
+const Map = ({map, style, viewport: {center, zoom}}: MapProps) => {
+    const mapContainer = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (map.current) {
+            return;
+        }
+
+        map.current = new maplibregl.Map({
+            container: mapContainer.current!,
+            style, 
+            center, 
+            zoom,
+        });
+    }, []);
+
+    return (
+        <div className="w-full h-full" ref={mapContainer} />
+    )
+}
+
+export default Map;
