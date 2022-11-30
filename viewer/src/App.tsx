88f97e3262f66e39ca3d76f274f3fc6f1f23b508
Matt Iannucci <matt.iannucci@rpsgroup.com>
2022-11-30 10:22:52 -0500

    Dealing with crashes but working nicely

--- viewer/src/App.tsx
+++ viewer/src/App.tsx
@@ -68,18 +68,21 @@ function App() {
     });
 
     map.current.addLayer({
-      id: sourceId, 
-      type: 'raster', 
-      source: sourceId, 
+      id: sourceId,
+      type: 'raster',
+      source: sourceId,
       paint: {}
     });
 
     return () => {
       console.log(`Removing layer: ${sourceId}`);
-      map.current?.removeLayer(sourceId); 
+      map.current?.removeLayer(sourceId);
       map.current?.removeSource(sourceId);
     }
   }, [selectedLayer]);
+
+
+  const selectedLayerData = selectedLayer ? datasets[selectedLayer.dataset][selectedLayer.variable] : undefined;
 
   return (
     <div className="h-screen w-screen flex flex-col overflow-hidden">
@@ -88,7 +91,7 @@ function App() {
         <span className="text-xl font-extrabold">xreds</span>
       </nav>
       <main className="flex flex-row flex-1">
-        <aside className={`flex flex-col h-full transition-all overflow-y-auto ${showSidebar ? 'w-full px-4 py-2' : 'w-0 px-0 py-0'} ${showSidebar ? 'md:w-1/3' : 'md:w-0'}`}>
+        <aside className={`absolute top-8 left-0 bottom-0 z-10 shadow-xl flex bg-white flex-col transition-all overflow-y-auto ${showSidebar ? 'w-full px-4 py-2' : 'w-0 px-0 py-0'} ${showSidebar ? 'md:w-1/3' : 'md:w-0'}`}>
           <h1 className="text-xl font-bold mb-4 px-1">Datasets</h1>
           {Object.keys(datasets).map(d => (
             <section key={d}>
@@ -96,7 +99,7 @@ function App() {
               {Object.keys(datasets[d]).map(v => (
                 <div key={d + v} className={`p-1 flex flex-row ${(selectedLayer?.dataset === d && selectedLayer.variable === v) ? 'bg-blue-100' : ''}`}>
                   <button
-                    className={`hover:text-blue-600 `}
+                    className={`hover:text-blue-600 text-start`}
                     onClick={() => {
                       if (selectedLayer) {
                         if (selectedLayer.dataset === d && selectedLayer.variable === v) {
@@ -126,6 +129,17 @@ function App() {
               zoom: 3,
             }} />
         </div>
+        {(selectedLayerData && selectedLayer) &&
+          <div className="absolute bottom-9 md:bottom-8 right-2 md:right-4 h-32 w-72 md:w-96 bg-white rounded-md bg-opacity-70 flex flex-col items-center content-center">
+            <span className="font-bold text-center">{selectedLayerData.Title} ({selectedLayerData.Units})</span>
+            <span>{selectedLayer.dataset}</span>
+            <div className=" w-full flex-1 flex flex-row items-center content-center justify-around font-bold">
+              <span>0</span>
+              <img className="rounded-md overflow-hidden w-64 md:w-80" src={`http://localhost:8090/datasets/${selectedLayer.dataset}/wms/?service=WMS&request=GetLegendGraphic&format=image/png&width=200&height=20&layers=${selectedLayer.variable}&styles=raster/default&colorscalerange=0,5`} />
+              <span>5</span>
+            </div>
+          </div>
+        }
       </main>
     </div>
   )
