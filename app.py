11aa44905879efcc018aa654e6a74a0621a4d6e0
Matthew Iannucci <matt.iannucci@rpsgroup.com>
2024-04-29 14:36:21 -0400

    Switch to dataset extensions for transforming datasets (#24)
    * Switch to dataset extensions for transforming datasets
    
    * ruff

--- app.py
+++ app.py
@@ -8,21 +8,19 @@ from xreds.plugins.size_plugin import SizePlugin
 from xreds.spastaticfiles import SPAStaticFiles
 from xreds.dataset_provider import DatasetProvider
 from xreds.plugins.subset_plugin import SubsetPlugin
-from xreds.plugins.vdatum_plugin import VDatumPlugin
 
 
 rest = xpublish.Rest(
     app_kws=dict(
-        title='XREDS',
-        description='XArray Environmental Data Services exposes environmental model data in common data formats for digestion in applications and notebooks',
-        openapi_url='/xreds.json'
+        title="XREDS",
+        description="XArray Environmental Data Services exposes environmental model data in common data formats for digestion in applications and notebooks",
+        openapi_url="/xreds.json",
     ),
     cache_kws=dict(available_bytes=1e9),
-    datasets=None
+    datasets=None,
 )
 
 rest.register_plugin(DatasetProvider())
-rest.register_plugin(VDatumPlugin())
 rest.register_plugin(SubsetPlugin())
 rest.register_plugin(SizePlugin())
 rest.register_plugin(ExportPlugin(netcdf_threshold=300))
@@ -31,23 +29,18 @@ app = rest.app
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
 app.mount("/", SPAStaticFiles(directory="./viewer/dist", html=True), name="viewer")
-app.root_path = os.environ.get('ROOT_PATH')
+app.root_path = os.environ.get("ROOT_PATH")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
 
     # When run directly, run in debug mode
-    uvicorn.run(
-        "app:app",
-        port = 8090,
-        reload = True,
-        log_level = 'debug'
-    )
+    uvicorn.run("app:app", port=8090, reload=True, log_level="debug")
--- xreds/dataset_provider.py
+++ xreds/dataset_provider.py
@@ -3,28 +3,35 @@ import datetime
 
 import fsspec
 import xarray as xr
+from pluggy import PluginManager
 
 from xpublish import Plugin, hookimpl
 
+from xreds.dataset_extension import DATASET_EXTENSION_PLUGIN_NAMESPACE
 from xreds.logging import logger
 from xreds.config import settings
 from xreds.utils import load_dataset
+from xreds.extensions import VDatumTransformationExtension
+
+
+dataset_extension_manager = PluginManager(DATASET_EXTENSION_PLUGIN_NAMESPACE)
+dataset_extension_manager.register(VDatumTransformationExtension, name="vdatum")
 
 
 class DatasetProvider(Plugin):
-    name: str = 'xreds_datasets'
+    name: str = "xreds_datasets"
     dataset_mapping: dict = {}
     datasets: dict = {}
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
-        if settings.datasets_mapping_file.startswith('s3'):
-            fs = fsspec.filesystem('s3', anon=True)
+        if settings.datasets_mapping_file.startswith("s3"):
+            fs = fsspec.filesystem("s3", anon=True)
         else:
-            fs = fsspec.filesystem('file')
+            fs = fsspec.filesystem("file")
 
-        with fs.open(settings.datasets_mapping_file, 'r') as f:
+        with fs.open(settings.datasets_mapping_file, "r") as f:
             self.dataset_mapping = json.load(f)
 
     @hookimpl
@@ -37,14 +44,14 @@ class DatasetProvider(Plugin):
 
         cached_ds = self.datasets.get(cache_key, None)
         if cached_ds:
-            if (datetime.datetime.now() - cached_ds['date']).seconds < (10 * 60):
-                logger.info(f'Using cached dataset for {dataset_id}')
-                return cached_ds['dataset']
+            if (datetime.datetime.now() - cached_ds["date"]).seconds < (10 * 60):
+                logger.info(f"Using cached dataset for {dataset_id}")
+                return cached_ds["dataset"]
             else:
-                logger.info(f'Cached dataset for {dataset_id} is stale, reloading...')
+                logger.info(f"Cached dataset for {dataset_id} is stale, reloading...")
                 self.datasets.pop(cache_key, None)
         else:
-            logger.info(f'No dataset found in cache for {dataset_id}, loading...')
+            logger.info(f"No dataset found in cache for {dataset_id}, loading...")
 
         dataset_spec = self.dataset_mapping[dataset_id]
         ds = load_dataset(dataset_spec)
@@ -52,14 +59,26 @@ class DatasetProvider(Plugin):
         if ds is None:
             raise ValueError(f"Dataset {dataset_id} not found")
 
-        self.datasets[cache_key] = {
-            'dataset': ds,
-            'date': datetime.datetime.now()
-        }
+        # There is a better way to do this probably, but this works well and is very simple
+        extensions = dataset_spec.get("extensions", {})
+        for ext_name, ext_config in extensions.items():
+            extension = dataset_extension_manager.get_plugin(ext_name)
+            if extension is None:
+                logger.error(
+                    f"Could not find extension {ext_name} for dataset {dataset_id}"
+                )
+                continue
+            else:
+                logger.info(f"Applying extension {ext_name} to dataset {dataset_id}")
+            ds = extension().transform_dataset(ds=ds, config=ext_config)
+
+        self.datasets[cache_key] = {"dataset": ds, "date": datetime.datetime.now()}
 
         if cache_key in self.datasets:
-            logger.info(f'Loaded and cached dataset for {dataset_id}')
+            logger.info(f"Loaded and cached dataset for {dataset_id}")
         else:
-            logger.info(f'Loaded dataset for {dataset_id}. Not cached due to size or current cache score')
+            logger.info(
+                f"Loaded dataset for {dataset_id}. Not cached due to size or current cache score"
+            )
 
         return ds
