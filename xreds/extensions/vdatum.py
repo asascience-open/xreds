72823b82f5d6e3ce222a0a6128f8b34e30f1306e
Matthew.Iannucci <matthew.iannucci@tetratech.com>
2024-05-01 10:24:35 -0400

    Make vdatum extension more configurable

--- xreds/extensions/vdatum.py
+++ xreds/extensions/vdatum.py
@@ -5,8 +5,15 @@ from xreds.logging import logger
 from xreds.utils import load_dataset
 
 
-def transform_mllw(ds: xr.Dataset, ds_vdatum: xr.Dataset) -> xr.Dataset:
-    """Transform the dataset to MLLW
+def transform_datum(
+    ds: xr.Dataset,
+    ds_vdatum: xr.Dataset,
+    target_zeta_var: str,
+    target_datum_var: str,
+    target_datum_name: str,
+    out_datum_var: str,
+) -> xr.Dataset:
+    """Transform the dataset to target datum
 
     Args:
         ds (xr.Dataset): The dataset to transform
@@ -15,8 +22,8 @@ def transform_mllw(ds: xr.Dataset, ds_vdatum: xr.Dataset) -> xr.Dataset:
     Returns:
         xr.Dataset: The transformed dataset
     """
-    datum = ds_vdatum["mllwtomsl"]
-    zeta = ds["zeta"]
+    datum = ds_vdatum[target_datum_var]
+    zeta = ds[target_zeta_var]
 
     datum_dims = [k for k in datum.dims]
     target_dims = [k for k in zeta.dims]
@@ -25,12 +32,12 @@ def transform_mllw(ds: xr.Dataset, ds_vdatum: xr.Dataset) -> xr.Dataset:
 
     # validate
     for o, k in new_dims.items():
-        assert ds_vdatum.mllwtomsl[o].shape == ds.zeta[k].shape
+        assert ds_vdatum[target_datum_var][o].shape == ds.zeta[k].shape
 
-    zeta_to_mllw = zeta + datum.rename(new_dims)
-    zeta_to_mllw = zeta_to_mllw.assign_attrs({"datum": "mllw"})
+    zeta_to_datum = zeta + datum.rename(new_dims)
+    zeta_to_datum = zeta_to_datum.assign_attrs({"datum": target_datum_name})
 
-    ds_transformed = ds.assign(zeta_mllw=zeta_to_mllw)
+    ds_transformed = ds.assign({out_datum_var: zeta_to_datum})
     return ds_transformed
 
 
@@ -61,6 +68,11 @@ class VDatumTransformationExtension(DatasetExtension):
                 f"Could not load vdatum dataset from {vdatum_file}. Skipping vdatum transformation"
             )
             return ds
+        
+        target_zeta_var = config.get("water_level_var", "zeta")
+        target_datum_var = config.get("vdatum_var", "igld85tolwd")
+        target_datum_name = config.get("vdatum_name", "igld85")
+        out_datum_var = f"{target_zeta_var}_{target_datum_name}"
 
-        ds_transformed = transform_mllw(ds, ds_vdatum)
+        ds_transformed = transform_datum(ds, ds_vdatum, target_zeta_var, target_datum_var, target_datum_name, out_datum_var)
         return ds_transformed
