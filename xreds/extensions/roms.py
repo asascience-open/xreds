5c2d74baa4ada33481d3529c70c9d7a0131e1f91
Matthew Iannucci <mpiannucci@gmail.com>
2024-06-11 10:52:39 -0400

    Update to work with surface velocities

--- xreds/extensions/roms.py
+++ xreds/extensions/roms.py
@@ -2,7 +2,7 @@ import numpy as np
 import xarray as xr
 
 from xreds.dataset_extension import DatasetExtension, hookimpl
-
+from xreds.logging import logger
 
 class ROMSExtension(DatasetExtension):
     """Transform ROMS currents to the global reference frame
@@ -21,73 +21,76 @@ class ROMSExtension(DatasetExtension):
     def transform_dataset(self, ds: xr.Dataset, config: dict) -> xr.Dataset:
         angle = ds.angle
 
-        if 's_rho' in ds.dims:
-            if 'temp' in ds:
+        default_da: xr.DataArray | None = None
+        if "s_rho" in ds.dims:
+            if "temp" in ds:
                 default_da = ds.temp
-            elif 'salt' in ds:
-                default_da = ds.salt
-            else:
-                raise ValueError('No temperature or salinity variable found in dataset. This needed for the ROMs transformation')
-        else:
-            if 'temp_sur' in ds:
+            if "temp_sur" in ds:
                 default_da = ds.temp_sur
-            elif 'salt_sur' in ds:
+            elif "salt" in ds:
+                default_da = ds.salt
+            elif "salt_sur" in ds:
                 default_da = ds.salt_sur
-            else:
-                raise ValueError('No temperature or salinity variable found in dataset. This needed for the ROMs transformation')
+
+        if default_da is None:
+            logger.warn("No default data array found in dataset. Skipping ROMS transformation")
+            return ds
 
         # Start with u
-        u = ds.u
+        u_name = "u_sur" if "u_sur" in ds else "u"
+        u = ds[u_name]
+
         u_rho = default_da.copy()
-        u_rho.name = 'u_rho'
-        u_rho.attrs['field'] = u.attrs['field']
-        u_rho.attrs['units'] = u.attrs['units']
-        u_rho.attrs['standard_name'] = u.attrs['standard_name']
-        u_rho.attrs['long_name'] = u.attrs['long_name']
+        u_rho.name = "u_rho"
+        u_rho.attrs["field"] = u.attrs["field"]
+        u_rho.attrs["units"] = u.attrs["units"]
+        u_rho.attrs["standard_name"] = u.attrs["standard_name"]
+        u_rho.attrs["long_name"] = u.attrs["long_name"]
 
         # Have to rename the dimensions to match the rho grid, otherwise
         # xarray will refuse
-        u_renamed = u.rename({
-            'xi_u': 'xi_rho',
-            'eta_u': 'eta_rho'
-        })
+        u_renamed = u.rename({"xi_u": "xi_rho", "eta_u": "eta_rho"})
 
-        u_rho[dict(xi_rho=slice(1, -1))] = 0.5 * (u_renamed[dict(xi_rho=slice(0, -1))] + u_renamed[dict(xi_rho=slice(1, None))])
+        u_rho[dict(xi_rho=slice(1, -1))] = 0.5 * (
+            u_renamed[dict(xi_rho=slice(0, -1))]
+            + u_renamed[dict(xi_rho=slice(1, None))]
+        )
         u_rho[dict(xi_rho=0)] = u_renamed[dict(xi_rho=0)]
         u_rho[dict(xi_rho=-1)] = u_renamed[dict(xi_rho=-1)]
 
         # Now do v
-        v = ds.v
+        v_name = "v_sur" if "v_sur" in ds else "v"
+        v = ds[v_name]
 
         v_rho = default_da.copy()
-        v_rho.name = 'v_rho'
-        v_rho.attrs['field'] = v.attrs['field']
-        v_rho.attrs['units'] = v.attrs['units']
-        v_rho.attrs['standard_name'] = v.attrs['standard_name']
-        v_rho.attrs['long_name'] = v.attrs['long_name']
+        v_rho.name = "v_rho"
+        v_rho.attrs["field"] = v.attrs["field"]
+        v_rho.attrs["units"] = v.attrs["units"]
+        v_rho.attrs["standard_name"] = v.attrs["standard_name"]
+        v_rho.attrs["long_name"] = v.attrs["long_name"]
 
         # Have to rename the dimensions to match the rho grid, otherwise
         # xarray will refuse
-        v_renamed = v.rename({
-            'xi_v': 'xi_rho',
-            'eta_v': 'eta_rho'
-        })
+        v_renamed = v.rename({"xi_v": "xi_rho", "eta_v": "eta_rho"})
 
-        v_rho[dict(eta_rho=slice(1, -1))] = 0.5 * (v_renamed[dict(eta_rho=slice(0, -1))] + v_renamed[dict(eta_rho=slice(1, None))])
+        v_rho[dict(eta_rho=slice(1, -1))] = 0.5 * (
+            v_renamed[dict(eta_rho=slice(0, -1))]
+            + v_renamed[dict(eta_rho=slice(1, None))]
+        )
         v_rho[dict(eta_rho=0)] = v_renamed[dict(eta_rho=0)]
         v_rho[dict(eta_rho=-1)] = v_renamed[dict(eta_rho=-1)]
 
         u_rotated = u_rho * np.cos(angle) - v_rho * np.sin(angle)
         u_rotated = u_rotated.assign_attrs(u_rho.attrs)
-        u_rotated.name = 'u_rotated'
-        u_rotated.attrs['long_name'] = 'u velocity rotated from ROMS grid'
+        u_rotated.name = f"{u_name}_rotated"
+        u_rotated.attrs["long_name"] = "u velocity rotated from ROMS grid"
 
         v_rotated = v_rho * np.cos(angle) + u_rho * np.sin(angle)
         v_rotated = v_rotated.assign_attrs(v_rho.attrs)
-        v_rotated.name = 'v_rotated'
-        v_rotated.attrs['long_name'] = 'v velocity rotated from ROMS grid'
+        v_rotated.name = f"{v_name}_rotated"
+        v_rotated.attrs["long_name"] = "v velocity rotated from ROMS grid"
 
-        ds['u_rotated'] = u_rotated
-        ds['v_rotated'] = v_rotated
+        ds[f"{u_name}_rotated"] = u_rotated
+        ds[f"{v_name}_rotated"] = v_rotated
 
         return ds
